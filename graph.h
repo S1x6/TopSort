a76c2ebd8ca25931061f1c5280ceda9772bae9af
S1x6 <v.korshunov@g.nsu.ru>
2017-04-13 20:02:20 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -2,20 +2,33 @@
 #define GRAPH_H
 
 #include "stdio.h"
+#include "malloc.h"
 
-#define COLOR_WHITE 0;
-#define COLOR_GRAY 1;
-#define COLOR_BLACK 2;
+#define COLOR_WHITE 0
+#define COLOR_GRAY 1
+#define COLOR_BLACK 2
 
-typedef struct _Vertex Vertex;
-typedef struct _Edge Edge;
-typedef struct _ListNode ListNode;
+typedef struct _Vertex {
+	int num;
+	char color;
+	struct _ListNode * list_to;
+} Vertex;
 
-Vertex pop(ListNode ** head);
-ListNode * push(Vertex data, ListNode *head);
+typedef struct _ListNode {
+    struct _Vertex * value;
+	struct _ListNode * next;
+} ListNode;
+
+Vertex * pop(ListNode ** head);
+ListNode * push(Vertex * data, ListNode *head);
 void clearList(ListNode ** head);
-
-
+ListNode * ListNodeC(Vertex * value);
+Vertex ** constructVertex(int vert_num);
+void writeSortedGraph(FILE * fout, Vertex ** v, int v_num);
+void printStack(FILE * fout, ListNode *stack);
+void processVertex(FILE * fout,Vertex * v, ListNode ** stack);
+Vertex * VertexC(int num, char color, ListNode * list_to);
+void destructVertex(Vertex * v);
+void destructList(ListNode * l);
 
 #endif
-
