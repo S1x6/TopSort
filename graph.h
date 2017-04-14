6be8a79be492390ea48b83543db610df45ff999d
S1x6 <v.korshunov@g.nsu.ru>
2017-04-14 17:10:23 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -8,27 +8,68 @@
 #define COLOR_GRAY 1
 #define COLOR_BLACK 2
 
+/**
+*	Структура, определяющая вершины графа
+*	Поля:	int num - номер вершины
+*		char color - цвет вершины (статус)
+*		ListNode list_to - список смежных вершин 
+*/
 typedef struct _Vertex {
 	int num;
 	char color;
 	struct _ListNode * list_to;
 } Vertex;
 
+/**
+*	Структура, определяющая список, содержащий вершины графа
+*	Поля:	Vertex * value - указатель на хранимую вершину
+		ListNode * next - указатель на следующий элемент списка
+*/
 typedef struct _ListNode {
-    struct _Vertex * value;
+    	struct _Vertex * value;
 	struct _ListNode * next;
 } ListNode;
-
 Vertex * pop(ListNode ** head);
 ListNode * push(Vertex * data, ListNode *head);
-void clearList(ListNode ** head);
+
+/**
+*	Конструктор элемента списка с заданным значением
+*	Аргументы:	Vertex * vertex - указатель на вершину-значение
+*	Возвращает: указатель на сформированный элемент списка
+*/
 ListNode * ListNodeC(Vertex * value);
+
+/**
+*	Конструктор массива всех вершин графа
+*	Аргументы: int vert_num - количество вершин в графе
+*	Возвращает: указатель на массив вершин
+*/
 Vertex ** constructVertex(int vert_num);
+
+/**
+*	Выводит в поток номера вершин графа в топологически отсортированном порядке
+*	Аргументы: FILE* fout - поток вывода
+*		   Vertex ** v - массив вершин графа
+*		   int v_num - количество вершин
+*/
 void writeSortedGraph(FILE * fout, Vertex ** v, int v_num);
-void printStack(FILE * fout, ListNode *stack);
-void processVertex(FILE * fout,Vertex * v, ListNode ** stack);
+
+/*private*/ void printStack(FILE * fout, ListNode *stack);
+
+/*private*/ void processVertex(FILE * fout,Vertex * v, ListNode ** stack);
+
+/**
+*	Конструктор вершины
+*	Аргументы: int num - номер вершины
+*		   char color - цвет вершины
+*		   ListNode * list_to - список смежных вершин
+*	Возвращает: указатель на созданную вершину
+*/
 Vertex * VertexC(int num, char color, ListNode * list_to);
+
+/* Деструктор вершины*/
 void destructVertex(Vertex * v);
+/* Деструктор списка*/
 void destructList(ListNode * l);
 
 #endif
